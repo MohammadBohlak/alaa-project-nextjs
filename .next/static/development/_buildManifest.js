8690b9421e8d1577c4bf8fa11e875475ebc01087
MohammadBohlak <abuomarcom4@gmail.com>
2024-09-15 10:50:35 +0300

    del

--- .next/static/development/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/customers/edit":["static\u002Fchunks\u002Fpages\u002Fcustomers\u002Fedit.js"],sortedPages:["\u002F_app","\u002F_error","\u002Fcustomers\u002Fedit"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
